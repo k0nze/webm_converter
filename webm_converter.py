19b2e2ce8bfc1179398b71eb7911cc14707eac01
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2021-07-13 10:57:26 +0200

    Added crf.

--- webm_converter.py
+++ webm_converter.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     out, _ = (
         ffmpeg
         .input('test.mov')
-        .output('test.webm', vcodec='libvpx-vp9', pix_fmt='yuva420p', bitrate='2M')
+        .output('test.webm', vcodec='libvpx-vp9', pix_fmt='yuva420p', crf='15', bitrate='2M')
         .run(capture_stdout=True)
     )
